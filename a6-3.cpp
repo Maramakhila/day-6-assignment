c255352a8650ff3bdf45f2b867f25251181e92dd
Maramakhila <130456749+Maramakhila@users.noreply.github.com>
2023-04-19 19:30:52 +0530

    Add files via upload

---
+++ a6-3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int factorial(int num);
+
+int main()
+{
+    int num;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d", num, factorial(num));
+
+    return 0;
+}
+
+int factorial(int num)
+{
+    if (num == 0)
+        return 1;
+    else
+        return num * factorial(num - 1);
+}
+
+
+
+
+
